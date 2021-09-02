1fbc9bcfed78306dd49ca0517c8ea081d485faa1
Birey Cikan <bireycikan@gmail.com>
2021-09-02 18:04:57 +0300

    implement single update operation for branches

--- routes/branches.js
+++ routes/branches.js
@@ -64,6 +64,51 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.post('/:id', async (req, res) => {
+
+  try {
+    const { id } = req.params;
+
+    if (_.isEmpty(req.body)) {
+      return res.status(400).json({
+        success: false,
+        message: "You must send fields object with field names and updated values in order to update!",
+        detail: "e.g: fields: { 'phone': '+902121234567' } - [name,phone,full_address,latitude,longitude] fields can be updated."
+      });
+    }
+
+    const { fields } = req.body;
+
+    const modelData = {
+      operation: 'UPDATE',
+      model: {
+        isMultiple: false,
+        name: 'Branches',
+        id: {
+          name: "branch_id",
+          value: id
+        },
+        fields: fields
+      }
+    };
+
+
+    await mysql({
+      method: 'POST',
+      data: modelData
+    })
+
+    res.status(200).json({ success: true, message: "Successfuly updated!" })
+  } catch (err) {
+    if (err.isAxiosError) {
+      const { response: { status, data } } = err;
+      res.status(status).send(`${data}`)
+    }
+    else {
+      res.status(400).send(`Something failed: ${err.message}`);
+    }
+  }
+})
 
 
 module.exports = router;
